6bd208df18ce8b24f2b270e336d0cf2bb4cd5634
talves <tony@3alves.com>
2021-01-29 11:11:30 -0800

    site init 🔥

--- www/site/src/pages/index.js
+++ www/site/src/pages/index.js
@@ -3,5 +3,6 @@ import { h } from "preact";
 export default (props) => (
   <div>
     <a href="https://github.com/talves/toast-template">talves/toast-template</a>
+    <pre>{JSON.stringify()}</pre>
   </div>
 );
--- www/site/toast.js
+++ www/site/toast.js
@@ -1,6 +1,39 @@
-import { sourceMdx } from "@toastdotdev/mdx";
+import {
+  sourceMdx,
+  fetchMdxFromDisk as fetchFilesFromDisk,
+} from "@toastdotdev/mdx";
+
+/* parses out the object into a data return object for all files */
+const getFilesObject = (files) => {
+  const data = {};
+  files.forEach((file) => {
+    const dirArr = file.filename.split("/");
+    const name = dirArr
+      .slice(dirArr.length - 1)[0]
+      .split(".")
+      .slice(0, -1)
+      .join(".");
+    data[name] =
+      typeof file.file === "string" ? JSON.parse(file.file) : file.file;
+  });
+  return data;
+};
 
 export const sourceData = async ({ setDataForSlug }) => {
+  /* gets the settings data from disk and writes out the json file to static path */
+  const settingsFiles = await fetchFilesFromDisk({
+    directory: "./data/settings",
+    extensions: ["json"],
+  });
+  const navigationFiles = await fetchFilesFromDisk({
+    directory: "./data/navigation",
+    extensions: ["json"],
+  });
+  const settings = getFilesObject(settingsFiles);
+  settings.navigation = getFilesObject(navigationFiles);
+  await setDataForSlug("/settings", { data: { ...settings } });
+
+  /* gets mdx content and sets the slug for the content */
   await sourceMdx({
     setDataForSlug,
     directory: "./content",
