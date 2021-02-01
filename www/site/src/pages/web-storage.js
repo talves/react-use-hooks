0540e944c3de02d5946260f59020f873f015959b
talves <tony@3alves.com>
2021-02-01 15:07:55 -0800

    revise example

--- www/site/src/pages/web-storage.js
+++ www/site/src/pages/web-storage.js
@@ -1,23 +1,48 @@
 import { h } from "preact";
+import { useState, useEffect } from "preact/compat";
 import { useWebStorage } from "@talves/use-web-storage";
 
 export default (props) => {
-  const [storage, setStorage] = useWebStorage(
+  const [storageType, setStorageType] = useState("sessionStorage");
+  const [storage, setStorage, setType] = useWebStorage(
     "test-data",
-    { site: "tony.alves.dev" }
-    // { type: "sessionStorage" }
+    { site: "tony.alves.dev" },
+    { type: storageType }
   );
+
+  useEffect(() => {
+    console.log(`type: ${storageType}`);
+    setType(storageType);
+  }, [storageType]);
 
   const handleChangeEvent = (e) => {
     setStorage({ site: e.target.value });
   };
 
+  const onChangeType = (event) => {
+    console.log(event.target.value);
+    setStorageType(event.target.value);
+  };
+
   return (
     <div>
-      <input
-        onChange={handleChangeEvent}
-        defaultValue={storage ? storage.site : ""}
-      />
+      <div onChange={onChangeType}>
+        <input
+          type="radio"
+          value="localStorage"
+          name="storageType"
+          checked={storageType === "localStorage"}
+        />
+        {" localStorage"}
+        <input
+          type="radio"
+          value="sessionStorage"
+          name="storageType"
+          checked={storageType === "sessionStorage"}
+        />
+        {" sessionStorage"}
+      </div>
+      <input onChange={handleChangeEvent} value={storage ? storage.site : ""} />
       {storage && <pre>{JSON.stringify(storage, null, 2)}</pre>}
     </div>
   );
