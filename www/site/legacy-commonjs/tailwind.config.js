5e01f9854acfc79b7398746baf808ec81032b75e
talves <tony@3alves.com>
2021-02-04 14:12:49 -0800

    tailwind import css
    add in some mdx styles for components

--- www/site/legacy-commonjs/tailwind.config.js
+++ www/site/legacy-commonjs/tailwind.config.js
@@ -7,6 +7,31 @@ module.exports = {
   },
   purge: ["./src/**/*.js", "./content/**/*.mdx"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      gray: {
+        100: "#f7fafc",
+        200: "#edf2f7",
+        300: "#e2e8f0",
+        400: "#cbd5e0",
+        500: "#a0aec0",
+        600: "#718096",
+        700: "#4a5568",
+        800: "#2d3748",
+        900: "#1a202c",
+      },
+      primary: {
+        100: "#6492A6",
+        200: "#59879b",
+        300: "#527b8e",
+        400: "#4A7082",
+        500: "#3C5A68",
+        600: "#344F5B",
+        700: "#2D444E",
+        800: "#253941",
+        900: "#1E2E34",
+      },
+    },
     customForms: (theme) => ({
       default: {
         input: {
--- www/site/src/page-wrapper.js
+++ www/site/src/page-wrapper.js
@@ -2,11 +2,16 @@ import { h } from "preact";
 import { MDXProvider } from "@mdx-js/preact";
 import { SiteDataProvider } from "./site-data-provider.js";
 import Layout from "./components/Layout/index.js";
+import { Box } from "./components/Box.js";
 
 const components = {
+  h1: (props) => <Box as="h1" {...props} class="heading h1-mdx" />,
+  h2: (props) => <Box as="h2" {...props} class="heading h2-mdx" />,
+  h3: (props) => <Box as="h3" {...props} class="heading h3-mdx" />,
+  h4: (props) => <Box as="h4" {...props} class="heading h4-mdx" />,
   codeblock: (props) => (
     <div
-      class="bg-gray-900"
+      class="bg-gray-900 p-2"
       dangerouslySetInnerHTML={{ __html: props.children }}
     />
   ),
