c5a461eba3ed512520de2d0a88243673e46fd916
talves <tony@3alves.com>
2021-02-05 16:31:43 -0800

    add jest react-testing-library

---
+++ packages/@talves/use-previous/index.test.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { renderHook, act } from "@testing-library/react-hooks/native";
+import { usePrevious } from "./src/index";
+
+test("should return undefined initial value and previous set value", () => {
+  const { result, rerender } = renderHook(() => {
+    const [value, setValue] = useState(0);
+    return {
+      value,
+      setValue,
+      prevValue: usePrevious(value),
+    };
+  });
+  expect(result.current.prevValue).toBe(undefined);
+
+  act(() => {
+    result.current.setValue(result.current.value + 1);
+  });
+  expect(result.current.prevValue).toBe(0);
+  expect(result.current.value).toBe(1);
+
+  act(() => {
+    result.current.setValue(22);
+  });
+  expect(result.current.prevValue).toBe(1);
+  expect(result.current.value).toBe(22);
+});
