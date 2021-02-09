3eec82cbf2e9b9af6fed12ff65e35d5ac08506c3
talves <tony@3alves.com>
2021-02-08 16:12:44 -0800

    favicons & manifest

--- www/site/src/components/Layout/index.js
+++ www/site/src/components/Layout/index.js
@@ -34,8 +34,8 @@ export default ({ children, ...props }) => {
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#ffc40d" />
-        <meta name="theme-color" content="#ffc40d"></meta>
+        <meta name="msapplication-TileColor" content="#5bbad5" />
+        <meta name="theme-color" content="#5bbad5"></meta>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Helmet>
