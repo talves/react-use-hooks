9706bc4d64556c7ed87aaedfeffd561df9c77aec
talves <tony@3alves.com>
2021-02-08 17:49:10 -0800

    add components

--- www/site/src/page-wrapper.js
+++ www/site/src/page-wrapper.js
@@ -3,12 +3,20 @@ import { MDXProvider } from "@mdx-js/preact";
 import { SiteDataProvider } from "./site-data-provider.js";
 import Layout from "./components/Layout/index.js";
 import { Box } from "./components/Box.js";
+import { OutsideLink } from "./components/buttons/OutsideLink.js";
 
 const components = {
+  OutsideLink,
+  Box,
   h1: (props) => <Box as="h1" {...props} class="heading h1-mdx" />,
   h2: (props) => <Box as="h2" {...props} class="heading h2-mdx" />,
   h3: (props) => <Box as="h3" {...props} class="heading h3-mdx" />,
   h4: (props) => <Box as="h4" {...props} class="heading h4-mdx" />,
+  p: (props) => <Box as="p" {...props} class="paragraph-mdx" />,
+  ul: (props) => <Box as="ul" {...props} class="ul-mdx" />,
+  blockquote: (props) => (
+    <Box as="blockquote" {...props} class="blockquote-mdx" />
+  ),
   codeblock: (props) => (
     <div
       class="bg-gray-900 p-2"
