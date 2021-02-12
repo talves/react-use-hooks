f434881cfff3db5006e0116c35992feebf9fbae8
talves <tony@3alves.com>
2021-02-11 17:31:16 -0800

    use esinstall

---
+++ www/site/esinstall.js
@@ -0,0 +1,17 @@
+export const specs = [
+  "@talves/use-component-size",
+  "@talves/use-previous",
+  "@talves/use-web-storage",
+  "preact",
+  "preact/hooks",
+  "@mdx-js/preact",
+  "react-helmet",
+  "preact/compat"
+  //   "gsap"
+];
+
+export const options = {
+  alias: {
+    react: "preact/compat"
+  }
+};
