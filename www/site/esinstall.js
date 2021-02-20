1f15859dc5231a5ebd07d97219b56c6bf181b431
talves <tony@3alves.com>
2021-02-20 14:38:10 -0800

    useWindowSize

--- www/site/esinstall.js
+++ www/site/esinstall.js
@@ -3,6 +3,7 @@ export const specs = [
   "@talves/use-component-size",
   "@talves/use-previous",
   "@talves/use-web-storage",
+  "@talves/use-window-size",
   "preact",
   "preact/compat",
   "preact/jsx-runtime",
