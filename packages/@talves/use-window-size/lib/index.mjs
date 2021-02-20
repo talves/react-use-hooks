07bc22a9f3d7c42fde1b8a4b2cc4d12ca80b3dbf
talves <tony@3alves.com>
2021-02-20 14:36:34 -0800

    check for window

--- packages/@talves/use-window-size/lib/index.mjs
+++ packages/@talves/use-window-size/lib/index.mjs
@@ -1,12 +1,14 @@
 import { useState, useLayoutEffect } from "react";
 
+function getWindowSize() {
+  const width = typeof window !== "undefined" ? window.innerWidth : 0;
+  const height = typeof window !== "undefined" ? window.innerHeight : 0;
+  return { width, height };
+}
+
 function useWindowSize() {
-  const [size, setSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  const handleResize = () =>
-    setSize({ width: window.innerWidth, height: window.innerHeight });
+  const [size, setSize] = useState(getWindowSize());
+  const handleResize = () => setSize(getWindowSize());
 
   useLayoutEffect(() => {
     window.addEventListener("resize", handleResize);
